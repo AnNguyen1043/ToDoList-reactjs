e822cdecb113841de97abc6e500f1b57b2b0e967
An Nguyen <an.ndg95@gmail.com>
2024-01-30 22:49:22 +0700

    newApp

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
 import React, { useState } from 'react'
 import "./App.css"
+import Header  from './component/Header';
+import TaskList from './component/TaskList';
+import AddForm from './component/AddForm';
 
 function App() {
     
 
-    const [count, setCount]=useState(0);
-    let color = (count>0 ) ? "green" : (count<0 )? "red" : "black";
+    const [tasks, setTasks]=useState([
+      {id:"task_1",title:"Learn JS", status:0},
+      {id:"task_2",title:"Create Todo List", status:1}
+    ]);
+ 
+
+    const [showIncomplete,setShowIncomplete]=useState(false);
     
-    const decrement= ()=> setCount(count-1);
-    const increment= ()=> setCount(count+1);
-    const resetCount= ()=> setCount(0);
-    
-    
-    
+    const [newTask, setNewTask]= useState("123");
+
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      if(newTask){
+        const task={
+          id:Date.now(),
+          title:newTask,
+          status: 0,
+        };
+        setTasks([...tasks,task]);
+        setNewTask("");
+      }
+      
+    };
+
+    const handleInputChange=(e)=>{
+      setNewTask(e.target.value);
+    };
+
+    const setTaskStatus= (taskId, status)=>{
+      setTasks(tasks.map(task=>{
+        if (task.id=== taskId){
+          return {...task,status: status? 1 :0};
+        }
+        return task;
+      }));
+    };
+
+    const removeTask =(taskId)=>{
+      setTasks(tasks.filter(task=>task.id!==taskId));
+    };
 
   return (
-   
 <div className="container">
-  <h1>Counter</h1>
-  <span style={{color}} id="value">{count}</span>
-  <div class="btn-group">
-    <button onClick={decrement} className="btn btn-de">DECREASE</button>
-    <button onClick={resetCount} className="btn btn-re">RESET</button>
-    <button onClick={increment} className="btn btn-in">INCREASE</button>
-  </div>
+  <Header/>
+  <TaskList tasks={tasks} showIncomplete={showIncomplete} setTaskStatus={setTaskStatus} removeTask={removeTask} setShowIncomplete={setShowIncomplete}/>
+  
+  <AddForm handleSubmit={handleSubmit} newTask={newTask} handleInputChange={handleInputChange}/>
+  
+  
 </div>
 
   )
